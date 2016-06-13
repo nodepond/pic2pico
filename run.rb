07ba755b5bd8a88c5dd74f62f51e544a75fa58f3
Nodepond <hello@nodepond.com>
2016-06-13 22:55:29 +0200

    small progress

--- run.rb
+++ run.rb
@@ -1,7 +1,24 @@
 require 'chunky_png'
 
+abort=""
+
 plz_convert_me = ChunkyPNG::Image.from_file('pic2pico-test.png')
 
 if plz_convert_me.width != 128 || plz_convert_me.height != 128
-  puts 'Sorry, currently only support for 128x128 pixel images.'
+  abort = 'Sorry, currently only support for 128x128 pixel images.'
+  puts abort
+end
+
+def convert()
+  for i in 0..128*128-1
+    puts i
+  end
+end
+
+# the "main"
+if abort.length > 0
+  puts 'we abort!'
+else
+  puts 'continue'
+  convert()
 end
