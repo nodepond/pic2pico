f1705640c491538f7761d3f1972986bbdd750e7f
Nodepond <hello@nodepond.com>
2016-06-13 23:04:38 +0200

    some commit

--- run.rb
+++ run.rb
@@ -6,16 +6,18 @@ require 'chunky_png'
 
 abort=""
 
-plz_convert_me = ChunkyPNG::Image.from_file('pic2pico-test.png')
+$plz_convert_me = ChunkyPNG::Image.from_file('pic2pico-test.png')
 
-if plz_convert_me.width != 128 || plz_convert_me.height != 128
+if $plz_convert_me.width != 128 || $plz_convert_me.height != 128
   abort = 'Sorry, currently only support for 128x128 pixel images.'
   puts abort
 end
 
 def convert()
   for i in 0..128*128-1
-    puts i
+    x = i%128
+    y = (i/128).floor()
+    puts $plz_convert_me[x,y]
   end
 end
 
@@ -23,6 +25,6 @@ end
 if abort.length > 0
   puts 'we abort!'
 else
-  puts 'continue'
   convert()
+  puts $plz_convert_me[1,1]
 end
